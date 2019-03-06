7a2cf7a460759a2a9836d4d0238cbf534f258b46
HiufaiLau <hiuailau.41@gmail.com>
2019-03-06 11:26:01 +0100

    angular with components and css

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,6 +3,7 @@ import { Server } from 'selenium-webdriver/safari';
 
 @Component({
   selector: 'app-servers',
+  // templateUrl: './servers.component.html', (external file, if more than 3 lines html code)
   template: `
   <app-server></app-server>
   <app-server></app-server>`,//change to template (each component need to have a template)
