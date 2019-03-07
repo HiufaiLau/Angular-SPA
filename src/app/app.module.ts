0516abf83a79eb96c403f0f152ccc99144a52ab9
HiufaiLau <hiuailau.41@gmail.com>
2019-03-07 15:38:10 +0100

    angular with input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';//ngModule decorator
-import {FormsModule} from '@angular/forms'; //this is a typescript feature, for tepyscript to know where the things are.
+import {FormsModule} from '@angular/forms'; 
+//this is a typescript feature, for tepyscript to know where the things are.
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -8,7 +9,8 @@ import { ServersComponent } from './servers/servers.component';//where to find t
 
 @NgModule({
   //ngModule decorator
-  // this is a feature understood by Angular since it's part of an Angular module.
+  // this is a feature understood by Angular 
+  //since it's part of an Angular module.
   //there are 4 properties that we set up to pass ngModule
   declarations: [
     AppComponent,
@@ -21,7 +23,9 @@ import { ServersComponent } from './servers/servers.component';//where to find t
     FormsModule,//tells Angular that to import some form features.
   ],
   providers: [],
-  bootstrap: [AppComponent]//telling angular which component should you be aware of at the point of time the whole application starts. 
+  bootstrap: [AppComponent]
+  //telling angular which component should you be aware of 
+  //at the point of time the whole application starts. 
   //which component you would basically recognize in the index.html file.
 })
-export class AppModule { }//empty typescript class
+export class AppModule {}//empty typescript class
